5362784c2f5ae2fdb0c51b6eae578ec6c9a8836e
ilya.podshivalov <ipodshivalov@getmobit.ru>
2019-01-17 18:33:31 +0300

    Refactoring in progress. This code doesn't work

--- src/output_helper.cpp
+++ src/output_helper.cpp
@@ -2,44 +2,59 @@
 #include <iostream>
 #include <ctime>
 #include <string>
+#include "file_output.h"
+
+OutputHelper::OutputHelper(const size_t poolSize) {
+    expandPool(poolSize);
+}
 
 OutputHelper::~OutputHelper() {
-    if (mLogIsOpened) {
-        mDumpFile.close();
+    reducePool(0);
+    for (size_t i = mPermanentOutputs.size(); i >= 0; --i) {
+        delete mPermanentOutputs[i];
     }
+}
+
+void OutputHelper::setPoolSize(const size_t newSize) {
+    if (newSize > mProcessingPool.size()) {
+        expandPool(newSize);
+    } else if (newSize < mProcessingPool.size()) {
+        reducePool(newSize);
+    }
+}
+
+void OutputHelper::addPermanentOutput(IOutputItem *outputItem) {
+    mPermanentOutputs.push_back(outputItem);
+}
+
+void OutputHelper::expandPool(const size_t newSize) {
+    mProcessingPool.resize(newSize);
+    for (size_t i = mProcessingPool.size(); i < newSize; ++i) {
+        mProcessingPool[i] = new FileOutput();
+    }
+}
+
+void OutputHelper::reducePool(const size_t newSize) {
+    for (size_t i = mProcessingPool.size(); i >= newSize; --i) {
+        delete mProcessingPool[i];
+    }
+    mProcessingPool.resize(newSize);
 }
 
 void OutputHelper::endl() {
-    std::cout << std::endl;
-    delayedInit();
-    if (mLogIsOpened) {
-        mDumpFile << std::endl;
-    }
+    ;
 }
 
 void OutputHelper::operator<<(const std::string &output) {
-    std::cout << output;
-    delayedInit();
-    if (mLogIsOpened) {
-        mDumpFile << output;
+    for (auto permanentItem: mPermanentOutputs) {
+        permanentItem << output;
+    }
+    while (true) {
+        auto available = std::find_if(mProcessingPool.begin(), mProcessingPool.end(), [](IOutputItem *item) { return item.isAvailable(); });
+        if (available != mProcessingPool.end()) {
+            poolItem << output;
+            break;
+        }
     }
 }
 
-void OutputHelper::delayedInit() {
-    if (!mLogIsOpened) {
-        mFileName = genFileName();
-        mDumpFile.open(mFileName, std::fstream::out | std::fstream::trunc);
-        mLogIsOpened = mDumpFile.is_open();
-    }
-}
-
-std::string OutputHelper::genFileName() {
-    std::string fileName("bulk");
-    fileName += std::to_string(std::time(nullptr));
-    fileName += ".log";
-    return fileName;
-}
-
-std::string OutputHelper::fileName() {
-    return mFileName;
-}
--- src/output_helper.h
+++ src/output_helper.h
@@ -1,21 +1,22 @@
 #pragma once
 #include <fstream>
+#include "output_item_interface.h"
+
 class string;
 
 class OutputHelper
 {
 public:
+    OutputHelper(const size_t poolSize = 1);
     ~OutputHelper();
     void endl();
     void operator<<(const std::string &o);
-    std::string fileName();
-
+    void setPoolSize(const size_t newSize);
+    void addPermanentOutput(IOutputItem *outputItem);
 private:
-    void delayedInit();
-    static std::string genFileName();
-
+    void expandPool(const size_t newSize);
+    void reducePool(const size_t newSize);
 private:
-    std::fstream mDumpFile;
-    bool mLogIsOpened = false;
-    std::string mFileName;
+    std::vector<IOutputItem *> mProcessingPool;
+    std::vector<IOutputItem *> mPermanentOutputs;
 };
